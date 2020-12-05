6e31ceaca6b1897756d6dda799859c25d66d2836
rStar <starlove124760@gmail.com>
2020-12-05 17:44:43 +0800

    feat : add SetUp new Character

--- Assets/Scripts/Tests/HealthTests.cs
+++ Assets/Scripts/Tests/HealthTests.cs
@@ -1,11 +1,22 @@
+#region
+
 using NUnit.Framework;
+
+#endregion
 
 public class HealthTests
 {
+    private Character _character;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _character = new Character();
+    }
+
     [Test]
     public void Hp100WhenCharacterNew()
     {
-        var character = new Character();
-        Assert.AreEqual( 100, character.GetHp());
+        Assert.AreEqual(100 , _character.GetHp());
     }
 }
