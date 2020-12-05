57c764e9ca5f5da3f2ebf21f13bbc46da95a6cea
rStar <starlove124760@gmail.com>
2020-12-05 18:02:19 +0800

    refactoring : Extract methods - TakeDamage, Heal , SetHp

--- Assets/Scripts/Tests/HealthTests.cs
+++ Assets/Scripts/Tests/HealthTests.cs
@@ -23,27 +23,30 @@ public class HealthTests
     [Test]
     public void Hp90WhenTakeDamage10()
     {
-        _character.TakeDamage(10);
+        TakeDamage(10);
         HpShouldBe(90);
     }
 
     [Test]
     public void Hp0WhenTakeDamage200()
     {
-        _character.TakeDamage(200);
+        TakeDamage(200);
         HpShouldBe(0);
     }
 
     [Test]
     public void Given_Hp90_When_Heal10_Then_Hp100()
     {
-        _character.SetHp(90);
-        _character.Heal(10);
+        SetHp(90);
+        Heal(10);
         HpShouldBe(100);
     }
 
-    private void HpShouldBe(int expected)
-    {
-        Assert.AreEqual(expected , _character.GetHp());
-    }
+    private void TakeDamage(int damage) => _character.TakeDamage(damage);
+
+    private void Heal(int hp) => _character.Heal(hp);
+
+    private void SetHp(int hp) => _character.SetHp(hp);
+
+    private void HpShouldBe(int expected) => Assert.AreEqual(expected , _character.GetHp());
 }
