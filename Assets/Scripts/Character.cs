3f38e75ac75df79ac4bd37647c6ab0a8c892e35a
rStar <starlove124760@gmail.com>
2020-12-09 05:16:17 +0800

    refactoring : inline method - ClampHpMin0Max100

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,31 +4,18 @@ public class Character
 {
     private int _currentHp;
 
-    // todo : use _currentHp = 100;
-    public Character(int initHp)
-    {
-        _currentHp = initHp;
-    }
+    public Character(int initHp) => _currentHp = initHp;
 
-    public int GetHp()
-    {
-        return _currentHp;
-    }
+    public int GetHp() => _currentHp;
 
-    // todo : inline variable
-    public void TakeDamage(int damage)
-    {
-        var calculatedHp = _currentHp - damage;
-        ClampHpMin0Max100(calculatedHp);
-    }
+    public void TakeDamage(int damage) => ClampHpMin0Max100(_currentHp - damage);
 
-    public void Heal(int hp)
-    {
-        var calculatedHp = _currentHp + hp;
-        ClampHpMin0Max100(calculatedHp);
-    }
+    public void Heal(int hp) => ClampHpMin0Max100(_currentHp + hp);
 
-    private void ClampHpMin0Max100(int calculatedHp) => _currentHp = Mathf.Clamp(calculatedHp , 0 , 100);
+    private void ClampHpMin0Max100(int calculatedHp)
+    {
+        _currentHp = Mathf.Clamp(calculatedHp , 0 , 100);
+    }
 
     public void SetHp(int hp) => _currentHp = hp;
 }
