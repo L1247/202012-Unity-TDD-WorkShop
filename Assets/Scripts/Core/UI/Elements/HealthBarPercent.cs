00a9a46e3a8ee250cda5d1a9168a7304364ec05a
rStar <starlove124760@gmail.com>
2020-12-25 04:53:19 +0800

    feat: add count text

--- Assets/Scripts/Core/UI/Elements/HealthBarPercent.cs
+++ Assets/Scripts/Core/UI/Elements/HealthBarPercent.cs
@@ -8,9 +8,14 @@ namespace Core.UI.Elements
         [SerializeField]
         private TMP_Text _tmpTextPercent;
 
+        [SerializeField]
+        private TMP_Text _tmpTextCount;
+
+
         public override void DisplayHealth(int amount , int maxValue)
         {
             base.DisplayHealth(amount , maxValue);
+            _tmpTextCount.text = $"X {amount        / 100}";
             var percent = (float)amount / maxValue * 100;
             _tmpTextPercent.text = $"{percent}%";
         }
